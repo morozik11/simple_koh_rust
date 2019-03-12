b0eada862877f97252e55534cb1abd8f5e3e8d3c
stas <stas@debi.debi>
2019-03-12 22:27:32 +0300

    error processing

--- koh/src/main.rs
+++ koh/src/main.rs
@@ -1,200 +1,228 @@
 extern crate rand;
+extern crate rustc_serialize;
 
-use std::fs::File;
-use std::io::{BufRead, BufReader};
-use std::process;
+
+use std::{fs, io, path::Path, error::Error};
+use io::{BufRead, BufReader};
+use fs::File;
 use rand::Rng;
+use rustc_serialize::json;
 
 
 fn main() {
-    
+	
     let f = "/home/stas/rust/files/test.csv";
-    
-    let mut vs = create_vectors(&f);
-    
-    let bool_ = check_leng_vectors(&vs);
-    if bool_ == false {
-        
-        process::exit(1);
-        
-    }
-
-    /* training */
-    let mut train_vectors = get_max_min_vectors(&vs);
-    let average = get_average_vectors(&train_vectors,&vs);
-    
-    train_vectors.extend(average);
-    
-    println!("{:?}",train_vectors);
-    println!("-------------------------------------------------------------------");
-    
-    let max_min = get_max_min(&train_vectors);
-    let a_b = get_a_b(&max_min);
-    wrap_normalize(&mut train_vectors, &a_b);
-
-    let mut new_vs = generate_rand_vs(train_vectors.len(),train_vectors[0].len());
-    wrap_training(&mut train_vectors, &mut new_vs);
-    /* training */
-    
-    /*classification*/
-    let max_min_ = get_max_min(&vs);
-    let a_b_ = get_a_b(&max_min_);
-    wrap_normalize(&mut vs, &a_b_);
-    classification(&mut new_vs, &mut vs);
-    /*classification*/
+	
+    let vs = create_vectors(&f);
+	
+    match vs {
+        Ok(v) => {	
+                
+            let bool_ = check_leng_vectors(&v);
+                    
+            if bool_ == true {
+                            
+                /* training */
+                let mut train_vectors = get_max_min_vectors(&v);
+                let average = get_average_vectors(&train_vectors,&v);
+                train_vectors.extend(average);
+                let max_min = get_max_min(&train_vectors);
+                let a_b = get_a_b(&max_min);
+                let mut normalized_train = wrap_normalize(&train_vectors, &a_b);
+                        
+                let mut new_vs = generate_rand_vs(normalized_train.len(),normalized_train[0].len());
+                wrap_training(&mut normalized_train, &mut new_vs);
+                /* training */
+                
+                /*classification*/
+                let max_min_ = get_max_min(&v);
+                let a_b_ = get_a_b(&max_min_);
+                let mut normalized = wrap_normalize(&v, &a_b_);
+                let result = classification(&mut new_vs, &mut normalized);
+                let encoded = json::encode(&result).unwrap();
+                println!("{}",encoded);
+                /*classification*/
+                    
+            } else {
+                 println!("error");            
+            }
+                    
+        },
+        Err(e) => {
+            println!("{:?}",e);        
+        }
+    };
     
 }
 
 fn check_leng_vectors(vectors: &Vec<Vec<f32>>) -> bool{
-    
-    let leng = vectors[0].len();
+	
     let mut result = true;
-    
-    for v in vectors {
-        
-        if v.len() < leng {
-            
-            result = false;
-            break;
-            
+	
+    match vectors.len() {
+        0 => {
+            return false;
+        },
+        1 => {
+            return false;
+        },
+        _ => {
+                    
         }
-        
     }
     
+    let leng = vectors[0].len();
+		
+    for v in vectors {
+		
+        match v.len() == leng {
+            false => {
+               return false;
+            },
+            true => {
+                            
+            }
+        }
+		
+    }
+	
     result
-    
+	
 }
 
-fn create_vectors(path: &str) -> Vec<Vec<f32>>{
+fn create_vectors(path: &str) -> Result<Vec<Vec<f32>>, Box<Error>>{
     
-    let mut file = File::open(path);
-
-    let mut f = match file {
-        Ok(file) => {
-            file = File::open(path);
-            file
-        },
-        Err(error) => {
-            file = File::open("/home/stas/rust/files/test.csv");
-            file
-        },
-    };
-
-    let f = BufReader::new(f);
+    let path = Path::new(path);
+    
+    if !path.is_file(){
+        return Err(Box::from("Файл не найден"));
+    }
+    
+    let file = File::open(path);
     
     let mut vectors = vec![];
     let mut vector = vec![];
     
-    for line in f.lines() {
-        
-        let line = line.expect("Unable to read line");
-        
-        if line != "" {
-
-            let words: Vec<&str> = line.split(",").collect();
-        
-            for w in words {
-            
-                let n:f32 = w.parse().expect("Not a number!");
-                vector.push(n);
-            
+    match file {
+        Ok(f) => {
+            let buff = BufReader::new(f);
+            for line in buff.lines() {
+                match line {
+                    Ok(l) => {
+                        let s = l.trim().to_string();
+                        if s != "" {
+                            
+                            let words: Vec<&str> = s.split(",").collect();
+                            for w in words {
+                                match w.parse::<f32>() {
+                                    Ok(n) => {
+                                        vector.push(n);
+                                    },
+                                    Err(e) => {
+                                        return Err(Box::from("Ошибка парсинга файла"));
+                                    }
+                                };
+                            }
+                            vectors.push(vector);
+                            vector = vec![];
+                            
+                        }            
+                    },
+                    Err(e) => {
+                        return Err(Box::from("Ошибка чтения файла"));
+                    },
+                };
             }
-        
-            vectors.push(vector);
-            vector = vec![];
-        
-        } else {
-            
-            panic!("Неверное число"); 
-
-        }
-    }
-
+        },
+        Err(e) => {
+            return Err(Box::from("Файл не найден"));
+        },
+    };
     
-    vectors
+    Ok(vectors)
     
 }
 
 fn get_average_vectors(vectors: &Vec<Vec<f32>>,vectors_for: &Vec<Vec<f32>>) -> Vec<Vec<f32>>{
-	
-	let mut list_summ = vec![];
-	
-	let new = vectors_for.clone();
-	
-	for vector in vectors {
+    
+    let mut list_summ = vec![];
+    
+    let new = vectors_for.clone();
+    
+    for vector in vectors {
+            
+        list_summ.push(summ(vector));
+            
+    }
+    
+    let max:f32;
+    let min:f32;
+    
+    if list_summ[0] > list_summ[1]{
+            
+        max = list_summ[0];
+        min = list_summ[1];
+            
+    } else {
 		
-		list_summ.push(summ(vector));
+	max = list_summ[1];
+	min = list_summ[0];
 		
-	}
+    }
 	
-	let max:f32;
-	let min:f32;
-	
-	if list_summ[0] > list_summ[1]{
-		
-		max = list_summ[0];
-		min = list_summ[1];
-		
-	} else {
-		
-		max = list_summ[1];
-		min = list_summ[0];
-		
-	}
-	
-	let average = (max + min) / 2.0;
-	
-	let max_average = (average + max) / 2.0;
-	let min_average = (average + min) / 2.0;
-	
-	let mut new_ = vec![];
-	
-	for vector in new {
-		
-		let summ_ = summ(&vector);
-		
-		if summ_ <= max_average && summ_ >= min_average {
-			
-			new_.push(vector);
-			
-		} 
-		
-	}
-	
-	new_
+    let average = (max + min) / 2.0;
+    
+    let max_average = (average + max) / 2.0;
+    let min_average = (average + min) / 2.0;
+    
+    let mut new_ = vec![];
+    
+    for vector in new {
+            
+        let summ_ = summ(&vector);
+            
+        if summ_ <= max_average && summ_ >= min_average {
+                    
+            new_.push(vector);
+                    
+        } 
+            
+    }
+    
+    new_
 	
 }
 
 fn get_max_min_vectors(vectors: &Vec<Vec<f32>>) -> Vec<Vec<f32>>{
 	
-	let mut list_summ = vec![];
-	
-	let new = vectors.clone();
-	
-	for vector in vectors {
-		
-		list_summ.push(summ(vector));
-		
-	}
-	
-	let min_key = min_elem_key(&list_summ);
-	let max_key = max_elem_key(&list_summ); 
-	
-	let mut new_ = vec![];
-	
-	let mut i = 0;
-	
-	for n in new{
-		
-		if i == min_key || i == max_key {
-			new_.push(n);
-		}
-		
-		i = i + 1;
-		
-	}
-	
-	new_
+    let mut list_summ = vec![];
+    
+    let new = vectors.clone();
+    
+    for vector in vectors {
+            
+        list_summ.push(summ(vector));
+            
+    }
+    
+    let min_key = min_elem_key(&list_summ);
+    let max_key = max_elem_key(&list_summ); 
+    
+    let mut new_ = vec![];
+    
+    let mut i = 0;
+    
+    for n in new{
+            
+            if i == min_key || i == max_key {
+                    new_.push(n);
+            }
+            
+            i = i + 1;
+            
+    }
+    
+    new_
 	
 }
 
@@ -236,15 +264,15 @@ fn  max_elem_key(vector: &Vec<f32>) -> usize{
 
 fn summ(vector: &Vec<f32>)->f32{
 	
-	let mut sum = 0.0;
-	
-	for elem in vector{
-		
-		sum = *elem + sum;
-		
-	}
-	
-	sum
+    let mut sum = 0.0;
+    
+    for elem in vector{
+            
+            sum = *elem + sum;
+            
+    }
+    
+    sum
 	
 }
 
@@ -333,14 +361,18 @@ fn normalize(vector: &Vec<f32>, a_b: &(f32,f32)) -> Vec<f32> {
     
 }
 
-fn wrap_normalize(vectors: &mut Vec<Vec<f32>>, a_b: &(f32,f32)){
+fn wrap_normalize(vectors: &Vec<Vec<f32>>, a_b: &(f32,f32)) -> Vec<Vec<f32>>{
+    
+    let mut normalized = vec![];
     
     for vector in vectors {
         
         let vector_ = normalize(vector,a_b);
-        *vector = vector_;
+        normalized.push(vector_);
         
     }
+    
+    normalized
     
 }
 
@@ -433,33 +465,43 @@ fn search_min(store: &Vec<(f32,usize)>) -> (f32,usize){
     
 }
 
-fn classification(vs_trained: &mut Vec<Vec<f32>>, vs: &mut Vec<Vec<f32>>){
+#[derive(RustcDecodable, RustcEncodable)]
+pub struct ClassVector {
+    vector: Vec<f32>,
+    class: usize,
+}
+
+fn classification(vs_trained: &mut Vec<Vec<f32>>, vs: &mut Vec<Vec<f32>>) -> Vec<ClassVector>{
+    
+    let clone = vs.clone();
     
     let mut i = 0;
     let leng = vs_trained.len();
     let mut store = vec![];
     
-    for v in vs {
+    let mut result = vec![];
+    
+    for v in clone {
         
         while i < leng{
             
-            let sum = euclidean(&vs_trained[i],v);
+            let sum = euclidean(&vs_trained[i],&v);
             store.push((sum,i));
             i = i + 1;
             
         }
         
         let mut smallest = search_min(&store);
+        let obj = ClassVector { vector : v,class : smallest.1 };
         
-        println!("{:?}",v);
-        println!("{:?}",vs_trained[smallest.1]);
-        println!("{:?}",smallest.1);
-        println!("==============================================================");
+        result.push(obj);
         
         store = vec![];
         i = 0;
         
     }
+    
+    result
     
 }
 
